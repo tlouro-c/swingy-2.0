43f3bdde73807a82b4105f3e1a43e8f45eeab0ca
tlouro-c <tomascorreia.lc@gmail.com>
2025-05-29 11:07:54 +0100

    Ready for eval

--- src/main/java/tc/tlouro_c/App.java
+++ src/main/java/tc/tlouro_c/App.java
@@ -7,12 +7,12 @@ public class App
 {
     public static void main( String[] args ) {
 
-//        if (args.length != 1) {
-//            showUsageAndExit();
-//        }
+       if (args.length != 1) {
+           showUsageAndExit();
+       }
 
-//        String modeArg = args[0].trim().toLowerCase();
-        var modeArg = "gui"; // todo dev only
+       String modeArg = args[0].trim().toLowerCase();
+    
         var mode = switch (modeArg) {
             case "gui" -> UIType.GUI;
             case "console" -> UIType.CLI;
